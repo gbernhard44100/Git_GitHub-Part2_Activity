8734fcfa34d68545dd3069223bfcc00daa09b640
gbernhard44100 <gaetan.bernhard@gmail.com>
2018-01-27 22:54:22 +0100

    Inclusion d'une nouvelle procédure pour connaître le taux de livraison

--- procedures.sql
+++ procedures.sql
@@ -16,3 +16,26 @@ right join Article on Panier.article_id=Article.id
 where delivered=0 and Article.jour=current_date()
 group by Commande.id
 order by Commande.livreur_alloue;
+
+-- Procédure pour coannaître le taux de livraison dans les temps --
+DELIMITER |
+Create procedure performance (in j date, out p_resultat decimal(4,2))
+begin
+-- calcul le nombre de livraison en retard (temps supérieur à 20 minutes) pour un jour donné --
+Set @retard_livraison:=(
+Select count(distinct Commande_id) from Commande inner join Panier on Commande.id=Panier.commande_id
+left join article on panier.article_id=article.id
+where temps>20 and Commande.temps is not null and Article.jour=j);
+
+-- calcul le nombre total de livraison sur un jour donné --
+Set @nb_livraison:=(
+Select count(distinct Commande_id) from Commande inner join Panier on Commande.id=Panier.commande_id
+left join Article on Panier.article_id=Article.id
+where Commande.temps is not null and Article.jour=j);
+
+-- calcul du taux de livraison dans les temps en pourcent --
+Set @taux_livraison:=100-@retard_livraison/@nb_livraison*100;
+select @taux_livraison into p_resultat;
+end |
+DELIMITER ;
+
