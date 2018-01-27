8ee1c2ab1c7a513cd68249c91de93811a0b4f7e5
gbernhard44100 <gaetan.bernhard@gmail.com>
2018-01-27 23:12:48 +0100

    Ajout du script php pour ma nouvelle application

---
+++ frontend.php
@@ -0,0 +1,71 @@
+<?php
+
+
+// Chargement des classes
+require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
+
+function listPosts()
+{
+    $postManager = new PostManager(); // Création d'un objet
+    $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
+
+    require('view/frontend/listPostsView.php');
+}
+
+function post()
+{
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.php');
+}
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+}
+
+function reviewComment($commentId)
+{       
+    $commentManager = new CommentManager();
+    $postManager = new PostManager();
+    
+    $comment_to_review = $commentManager->getComment($commentId);
+    $affected_comment = $comment_to_review->fetch();
+    
+    $post = $postManager->getPost($affected_comment['post_id']);
+    $comments = $commentManager->getComment($affected_comment['post_id']);
+
+    require('view/frontend/modifyCommentView.php');
+}
+
+function modifyComment($commentId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+    $postManager = new PostManager();
+    
+    $affectedLines = $commentManager->modifyComment($commentId, $author, $comment);
+    
+    $modifiedcomment = $commentManager->getComment($commentId);
+    $affectedcomment = $modifiedcomment->fetch();
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible de modifier le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $affectedcomment['post_id']);
+    }
+}
